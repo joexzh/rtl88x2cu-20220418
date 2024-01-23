9f8bda54e4808ceb2b7c92788f33df585e894496
Joe Tse <xiezhenhao@gmail.com>
2024-01-24 00:02:40 +0800

    dkms remove: add removee path /usr/src/rtl88x2cu-5.14.0.3

--- dkms-remove.sh
+++ dkms-remove.sh
@@ -5,7 +5,7 @@ PACKAGE_NAME=rtl88x2cu
 PACKAGE_VER=5.14.0.3
 
 sudo modprobe -rv 88x2cu
-sudo dkms remove -m $PACKAGE_NAME -v 5.14.0.3
-sudo rm -rf /usr/src/$PACKAGE_NAME-$PACKAGE_VER
+sudo dkms remove -m $PACKAGE_NAME -v $PACKAGE_VER
+sudo rm -rf "/usr/src/$PACKAGE_NAME-$PACKAGE_VER"
 
 echo "The driver $PACKAGE_NAME has been uninstalled."
